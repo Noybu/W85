ba4ff0ae9d3ddf08a9ca31814350b20e947a6550
ido <navarro.ido@gmail.com>
2019-07-15 18:09:48 +0300

    w

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php include_once("header.php"); ?>
 
-<div>
-    <h1>כותרת</h1>
-</div>
+<main >
 
+    <div class="container">
+        <div class="row" style="direction:rtl;">
+            <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto">
+                <h1>מימון המונים לתשתיות עירוניות</h1>
+                <p>ברוכים הבאים למיזם שיחולל מהפכה בתחום התשתיות העירוניות</p>
+                <p>חולמים על ספסל ברחוב? רוצים מגרש ציבורי חדש?</p>
+                <p>מהיום זה אפשרי! </p>
+                <button class="actionButton"><a href="#">התחילו</a></button>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto"><img src="images/undraw_having_fun_iais.svg" alt="" width="100%"></div>
+        </div>
+
+        <div class="row">
+            <h1>איך זה עובד?</h1>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto"></div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto"></div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto"></div>
+            <div>
+
+            </div>
+
+            <div class="row">
+                <h1>פרויקטים אחרונים</h1>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
+            </div>
+        </div>
+
+
+</main>
 <?php include_once("footer.php"); ?>
