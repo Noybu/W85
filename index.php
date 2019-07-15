2fe53daa7f0906187f687321c52c0ac4aaaceab2
ido <navarro.ido@gmail.com>
2019-07-16 00:25:04 +0300

    fix home

--- index.php
+++ index.php
@@ -3,52 +3,64 @@
 <main>
 
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto">
-                <h1>מימון המונים לתשתיות עירוניות</h1>
-                <p>ברוכים הבאים למיזם שיחולל מהפכה בתחום התשתיות העירוניות</p>
-                <p>חולמים על ספסל ברחוב? רוצים מגרש ציבורי חדש?</p>
-                <p>מהיום זה אפשרי! </p>
-                <button class="actionButton">התחילו להשפיע על העיר שלכם</button>
+        <section>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto">
+                    <h1 class="title">מימון המונים לתשתיות עירוניות</h1>
+                    <p class="subtitle">ברוכים הבאים למיזם שיחולל מהפכה בתחום התשתיות העירוניות</p>
+                    <p class="subtitle">חולמים על ספסל ברחוב? רוצים מגרש ציבורי חדש?</p>
+                    <p class="subtitle">מהיום זה אפשרי! </p>
+                    <button class="actionButton">התחילו להשפיע על העיר שלכם</button>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto"><img src="images/undraw_having_fun_iais.svg" alt="" width="100%"></div>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-1 col-xs-1 mx-auto"><img src="images/undraw_having_fun_iais.svg" alt="" width="100%"></div>
-        </div>
+        </section>
 
-        <div class="row">
-            <h1>איך זה עובד?</h1>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto">
-                <h1>1</h1>
-                <img src="" alt="">
-                <h2>שלב 1</h2>
-                <p>בשלב זה בלה בלה בלה </p>
+        <section>
+            <div class="row">
+                <h1>איך זה עובד?</h1>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto">
-            <h1>1</h1>
-                <img src="" alt="">
-                <h2>שלב 1</h2>
-                <p>בשלב זה בלה בלה בלה </p>
+            <div class="row sm">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto center">
+                    <h1 class="right level">1</h1>
+                    <img src="https://idona.mtacloud.co.il/Includes/img/box1.jpg" alt="">
+                    <h2>שלב 1</h2>
+                    <p>בשלב זה בלה בלה בלה </p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto center">
+                    <h1 class="right level">2</h1>
+                    <img src="https://idona.mtacloud.co.il/Includes/img/box3.jpg" alt="">
+                    <h2>שלב 1</h2>
+                    <p>בשלב זה בלה בלה בלה </p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto center">
+                    <h1 class="right level">3</h1>
+                    <img src="https://idona.mtacloud.co.il/Includes/img/box2.jpg" alt="">
+                    <h2>שלב 1</h2>
+                    <p>בשלב זה בלה בלה בלה </p>
+                </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mx-auto">
-            <h1>1</h1>
-                <img src="" alt="">
-                <h2>שלב 1</h2>
-                <p>בשלב זה בלה בלה בלה </p>
+            <div class="row center sm">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto center">
+                    <button class="actionButton">התחילו עכשיו</button>
+                </div>
+
             </div>
-            <button class="actionButton">התחילו עכשיו</button>
-        <div>
+        </section>
 
 
+        <section>
+            <div class="row">
 
-         <div class="row">
-
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto">
-                        <h1>פרויקטים אחרונים</h1>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto"></div>
-         </div>
-     </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto">
+                    <h1>פרויקטים אחרונים</h1>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto "></div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto "></div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 mx-auto "></div>
+            </div>
+    </div>
+    </section>
 
 
 </main>
