b30e7ad2b6c3cdbc4ad305f6478697a66b1a27a9
Noybu <48063366+Noybu@users.noreply.github.com>
2019-07-18 18:28:44 +0300

    add

---
+++ JS/newProject.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var formInputs = $('input[type=text], textarea, select');
+	formInputs.focus(function() {
+       $(this).parent().children('.formLabel').addClass('formTop');
+	});
+	formInputs.focusout(function() {
+		if ($.trim($(this).val()).length == 0){
+		$(this).parent().children('.formLabel').removeClass('formTop');
+		}
+	});
+	$('.formLabel').click(function(){
+		 $(this).parent().children('.form-style').focus();
+	});
+});
