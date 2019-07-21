aac8855cdf203f7bca4cf30ff3de62d6910b0116
ido <navarro.ido@gmail.com>
2019-07-21 19:33:10 +0300

    u

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <main>
 
-    <div class="container m-0">
+    <div class="container">
         <section>
             <div class="row sm">
                 <div class="col-lg-6 col-md-6 col-sm-10 col-xs-10 mx-auto">
@@ -57,8 +57,8 @@
              <div class="row">
                 <h1>פרויקטים אחרונים</h1>
             </div>
-            <div class="row sm m-0">
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-10 mx-auto card">
+            <div class="row sm">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10 mx-auto card">
                     <div class="pictureCard" style="background-image:url('https://idona.mtacloud.co.il/Includes/img/box2.jpg');"></div>      
                     <div class="descCard">
                         <h3>פרויקט1</h3>
@@ -79,7 +79,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-10 mx-auto card">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10 mx-auto card">
+
                     <div class="pictureCard" style="background-image:url('https://idona.mtacloud.co.il/Includes/img/box2.jpg');"></div>      
                     <div class="descCard">
                         <h3>פרויקט1</h3>
@@ -94,12 +95,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-10 mx-auto card">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10 mx-auto card">
                     <div class="pictureCard" style="background-image:url('https://idona.mtacloud.co.il/Includes/img/box2.jpg');"></div>      
                     <div class="descCard">
                         <h3>פרויקט1</h3>
-                        <div class="poly">aaaaaa
-                        </div>
+
                         <p>1234kjkfjfkgjfkgkfdjkfdgjkfdjgkfdkgjfdkgjfdkgjdjkg fdjsk fkjg jkgds jkgdsn klfjsd56</p>
                         <div class="bar">
                             <p style="text-align:right; font-size:14px; margin-bottom:0px; color:#36ba2f">1000<i class="fas fa-shekel-sign"></i></p>
