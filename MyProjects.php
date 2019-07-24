741931227e34a2991ee31d31853d2d042b28f05f
Noybu <48063366+Noybu@users.noreply.github.com>
2019-07-24 12:33:19 +0300

    add+update

--- MyProjects.php
+++ MyProjects.php
@@ -1,27 +1,35 @@
 <?php include_once("header.php"); ?>
+<link rel="stylesheet" type="text/css" href="CSS\myProject.css">
 <main>
 
-<section style="width:70%;">
+<section>
     <div class="row">
-        <div class="offset-lg-1 col-lg-3 col-md-4 col-sm-6 col-xs-10 mx-auto card">
-            <div class="pictureCard" style="background-image:url('https://idona.mtacloud.co.il/Includes/img/box2.jpg');"></div>
-            <div class="descCard">
+        <div class="card2">
+            <div class="pictureCard2" style="background-image:url('https://idona.mtacloud.co.il/Includes/img/box2.jpg');"></div>
+            <div class="descCard2">
                 <h3>פרויקט1</h3>
-                <p>1234kjkfjfkgjfkgkfdjkfdgjkfdjgkfdkgjfdkgjfdkgjdjkg fdjsk fkjg jkgds jkgdsn klfjsd56</p>
-
-                <div class="poly status-c">ממתין לאישור</div>
-                <div class="poly status-b">ממתין למכרז</div>
-                <div class="poly status-a">ממתין למימון</div>
-                <div class="poly status-short status-a ">בביצוע</div>
-                <div class="poly status-short status-a">הושלם</div>
-                <div class="bar">
+                <p class="loc">מיקום: רחוב עזה תל אביב-יפו</p>
+                <div id="allStatus">
+                    <div class="poly status-c">ממתין לאישור</div>
+                    <div class="poly status-b">ממתין למכרז</div>
+                    <div class="poly status-a">ממתין למימון</div>
+                    <div class="poly status-short status-a ">בביצוע</div>
+                    <div class="poly status-short status-a">הושלם</div>
+                </div>
+                <div class="bar2">
                     <p style="text-align:right; font-size:14px; margin-bottom:0px; color:#36ba2f">1000<i class="fas fa-shekel-sign"></i></p>
-                    <div id="mainBar">
+                    <div id="mainBar2">
                         <div style="width:40%;background-color:red;border-radius: 20px; ">
                             40
                         </div>
                     </div>
                 </div>
+            </div>
+            <div id="cardFooter">
+                <p id="share">שיתוף:</p>
+                <p class="icon" id="whatsapp"><i class="fab fa-whatsapp"></i></p>
+                <p class="icon" id="facebook"><i class="fab fa-facebook-square"></i></p>
+                <p id="view">לצפייה<i class="far fa-eye"></i></p>
             </div>
         </div>
     </div>
