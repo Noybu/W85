c16cfd5139b348759f1a1538e2eecad7b1825bc8
Asaf <asafybish@gmail.com>
2019-08-03 13:55:25 +0300

    UP

--- index.php
+++ index.php
@@ -72,6 +72,7 @@
                         <h3><?php echo getProjectType($arrProjects[$i]->projecttype);?></h3>
                         <div class="<?php echo getStatusColor($arrProjects[$i]->projectstatus);?> status"><?php echo getProjectStatus($arrProjects[$i]->projectstatus); ?></div>
                         <p><?php echo $arrProjects[$i]->description; ?></p>
+                        <div><a href="projectFund.php?projectid=<?php echo $arrProjects[$i]->projectid;?>">לצפייה</a></div>
                         <div class="bar">
                             <p style="text-align:right; font-size:14px; margin-bottom:0px; color:#36ba2f"><?php echo $arrProjects[$i]->projectcost;?><i class="fas fa-shekel-sign"></i></p>
                             <div id="mainBar"> 
@@ -81,7 +82,6 @@
                             </div>
                         </div>
                     </div>
-                    
                 </div>
             <?php
             }
