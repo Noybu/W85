6d6f1a62d7ff3702f0f02ed6a125bb54ccdda00d
Asaf <asafybish@gmail.com>
2019-08-07 17:02:29 +0300

    u

--- newProject.php
+++ newProject.php
@@ -39,26 +39,26 @@ if (isset($_POST['submit'])) {
                 <div id="locationField">
                     <div class="form-item">
                         <p class="formLabel formTop">כתובת מלאה</p>
-                        <input required type="text" name="longloc" class="form-style" id="autocomplete" onFocus="geolocate()" />
+                        <input required type="text" name="longloc" class="form-style" id="autocomplete" onFocus="geolocate()" onfocusout="check()" />
                     </div>
                 </div>
                 <div class="form-item">
-                    <p class="formLabel formTop">עיר</p>
-                    <input required type="text" name="locCity" class="form-style" id="locality" />
+                    
+                    <input required type="hidden" name="locCity" class="form-style" id="locality" />
                 </div>
                 <div class="form-item">
-                    <p class="formLabel formTop">רחוב</p>
-                    <input required type="text" name="locStreet" class="form-style" id="route"  />
+                    
+                    <input required type="hidden" name="locStreet" class="form-style" id="route" />
                 </div>
                 <div class="form-item">
-                    <p class="formLabel formTop">מספר</p>
-                    <input required type="text" name="locNum" class="form-style" id="street_number" />
+                    
+                    <input required type="hidden" name="locNum" class="form-style" id="street_number" />
                 </div>
                 <div class="form-item">
                     <input type="hidden" name="userID" value="1111" />
                 </div>
                 <div class="form-item">
-                    <input type="submit" class="login" name="submit" value="שלח">
+                    <input type="submit" class="loginoff" id="send" name="submit" value="שלח" disabled>
                 </div>
             </div>
         </form>
@@ -66,73 +66,99 @@ if (isset($_POST['submit'])) {
 
 
 </main>
+<script>
+    function check() {
+        var street_num = document.getElementById("street_number").value;
+        var street = document.getElementById("route").value;
+        var city = document.getElementById("locality").value;
+        var send = document.getElementById("send");
+
+        if (street_num || street || city) {
+            alert("Empty fields");
+            return false;
+        }
+
+        send.removeAttribute('disabled');
+        send.classList.remove("loginoff");
+        send.classList.add("login");
+     
+
+
+        return true;
+    }
+</script>
+
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK1iswODlN7nZQbJTB2viQH03KTUomNiE&&libraries=places&callback=initAutocomplete" async defer></script>
 <script>
-// This sample uses the Autocomplete widget to help the user select a
-// place, then it retrieves the address components associated with that
-// place, and then it populates the form fields with those details.
-// This sample requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script
-// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+    // This sample uses the Autocomplete widget to help the user select a
+    // place, then it retrieves the address components associated with that
+    // place, and then it populates the form fields with those details.
+    // This sample requires the Places library. Include the libraries=places
+    // parameter when you first load the API. For example:
+    // <script
+    // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-var placeSearch, autocomplete;
+    var placeSearch, autocomplete;
 
-var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
-};
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+    };
 
-function initAutocomplete() {
-  // Create the autocomplete object, restricting the search predictions to
-  // geographical location types.
-  autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autocomplete'), {types: ['geocode']});
+    function initAutocomplete() {
+        // Create the autocomplete object, restricting the search predictions to
+        // geographical location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            document.getElementById('autocomplete'), {
+                types: ['geocode']
+            });
 
-  // Avoid paying for data that you don't need by restricting the set of
-  // place fields that are returned to just the address components.
-  autocomplete.setFields(['address_component']);
+        // Avoid paying for data that you don't need by restricting the set of
+        // place fields that are returned to just the address components.
+        autocomplete.setFields(['address_component']);
 
-  // When the user selects an address from the drop-down, populate the
-  // address fields in the form.
-  autocomplete.addListener('place_changed', fillInAddress);
-}
-
-function fillInAddress() {
-  // Get the place details from the autocomplete object.
-  var place = autocomplete.getPlace();
-
-
-
-  // Get each component of the address from the place details,
-  // and then fill-in the corresponding field on the form.
-  for (var i = 0; i < place.address_components.length; i++) {
-    var addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      var val = place.address_components[i][componentForm[addressType]];
-      document.getElementById(addressType).value = val;
+        // When the user selects an address from the drop-down, populate the
+        // address fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
     }
-  }
-}
 
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle(
-          {center: geolocation, radius: position.coords.accuracy});
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
-}
+    function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+
+
+        // Get each component of the address from the place details,
+        // and then fill-in the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+    }
+
+    // Bias the autocomplete object to the user's geographical location,
+    // as supplied by the browser's 'navigator.geolocation' object.
+    function geolocate() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+            });
+        }
+    }
 </script>
 <?php include_once("footer.php"); ?>
