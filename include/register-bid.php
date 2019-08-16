9961596b8b0167df99569524e1d0d214499bdf13
ido <navarro.ido@gmail.com>
2019-08-16 15:18:59 +0300

    u

--- include/register-bid.php
+++ include/register-bid.php
@@ -6,7 +6,7 @@ require_once 'BLL.php';
 $offerprice = $_POST['offerprice'];
 $offerdate = $_POST['offerdate'];
 $projectid =  $_POST['projectid'];
-$servicemanid = $_POST['servicemanid'];
+$servicemanid = $_POST['service'];
 
 addNewBidOffer($offerprice,$offerdate,$projectid,$servicemanid);
 header("Location: ../projectFund?projectid=$projectid");
