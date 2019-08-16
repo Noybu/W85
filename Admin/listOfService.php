f3a3f354f3efe4a9404421b87cade13ceaccd8cf
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-16 12:59:45 +0300

    u

--- Admin/listOfService.php
+++ Admin/listOfService.php
@@ -55,10 +55,7 @@
                     <button onclick="window.location.href='../login/uploadFiles/<?php echo $arrServiceMan[$i]->idfile;?>'">ת.ז</button>
                     <button onclick="window.location.href='../login/uploadFiles/<?php echo $arrServiceMan[$i]->proffile;?>'">ת.עוסק</button>
                 </td>
-                <td style="text-align:center;">  
-                    <button onclick="window.location.href='../include/update.php?type=serviceman&serviceid=<?php echo $arrServiceMan[$i]->$id;?>&status=1'" style="color:green;"><i class="far fa-thumbs-up"></i></button>
-                    <button onclick="window.location.href='../include/update.php?type=serviceman&serviceid=<?php echo $arrServiceMan[$i]->$id;?>&status=2'" style="color:red;"><i class="far fa-window-close"></i></button>
-                </td>
+               
             </tr>
             <?php
              }
