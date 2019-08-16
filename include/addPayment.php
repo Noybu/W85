b535203a28c871cfb3d84044e4f601eb46b3515d
ido <navarro.ido@gmail.com>
2019-08-16 15:36:17 +0300

    u

---
+++ include/addPayment.php
@@ -0,0 +1,12 @@
+<?php
+//require_once 'DAL.php';
+require_once 'BLL.php';
+
+
+$price = $_POST['price'];
+$projectid =  $_POST['projectid'];
+
+
+updateCurrentPrice($projectid,$price);
+header("Location: ../projectFund.php?projectid=$projectid");
+ ?>
