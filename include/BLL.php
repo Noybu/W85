c49e1354d5ae5889b4ae713ebc0b04049b2338b8
ido <navarro.ido@gmail.com>
2019-08-18 16:40:11 +0300

    u

--- include/BLL.php
+++ include/BLL.php
@@ -303,6 +303,13 @@ function get_user_type($id)
     return get_object($sql)->type;
 }
 
+//מקבל אם משתמש מאושר
+function getServiceApproved($id)
+{
+    $sql = "select approved from users where id = '$id'";
+    return get_object($sql)->type;
+}
+
 //בודק אם משתמש קיים כבר במערכת
 function is_username_exist($userName)
 {
