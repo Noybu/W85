4639a1135ccbb43ae58c0a2794bec7ce40c81e14
ido <navarro.ido@gmail.com>
2019-08-18 18:58:15 +0300

    u

--- include/addPayment.php
+++ include/addPayment.php
@@ -1,12 +1,27 @@
 <?php
-//require_once 'DAL.php';
-require_once 'BLL.php';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($ch, CURLOPT_POSTFIELDS,"cmd=_notify-validate".http_build_query($_POST));
+$response = curl_exec($ch);
+curl_close($ch);
+
+file_put_contents("log.txt",$response);
 
 
-//$price = $_POST['price'];
-$projectid =  $_POST['projectid'];
 
 
-updateCurrentPrice($projectid,$price);
-header("Location: ../projectFund.php?projectid=$projectid");
+// //require_once 'DAL.php';
+// require_once 'BLL.php';
+
+
+// //$price = $_POST['price'];
+// $projectid =  $_POST['projectid'];
+
+
+// updateCurrentPrice($projectid,$price);
+// header("Location: ../projectFund.php?projectid=$projectid");
  ?>
