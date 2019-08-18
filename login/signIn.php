997764a15d254cf567c80192c3d9a43af6dd1951
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-18 17:22:50 +0300

    u

--- login/signIn.php
+++ login/signIn.php
@@ -47,7 +47,7 @@
             <p>נא להזין תעודת זהות המכילה 9 ספרות
             וסיסמא כפי שנמסרו בעת הרישום</p>
             <p>אלפי פרויקטים מחכים לכם במערכת !!!</p>
-            <a href="signUp.php"><h3>אתה עוד לא רשום?? לחץ כאן כדי להרשם</h3></a>
+            <a href="signUp.php"><h4 style=" text-decoration:none; color:white;">אתה עוד לא רשום?? לחץ כאן כדי להרשם</h4></a>
         </div>
     </section>
       <div id="mainDiv">
