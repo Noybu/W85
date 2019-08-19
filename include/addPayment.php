780bf4f78db2aec4cf40dee600a98539af5704e3
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-19 14:50:59 +0300

    u

--- include/addPayment.php
+++ include/addPayment.php
@@ -1,92 +1,196 @@
 <?php
-    require_once 'DAL.php';
-    require_once 'BLL.php';
-    use PHPMailer\PHPMailer\PHPMailer;
-    require "mailer/PHPMailer.php";
-    require "mailer/Exception.php";
-
-$ch = curl_init();
-
-$clientId = "AdFIOOghKH7yHwoLS6g6JeZAczNBeNkUbt_hyMcLL0g6w5dfIUscHFhv-0IhxE6Y6IYaFEOUDi9voT_C";
-$secret = "EHkHKQVIZtdF2B2KbjwInXFR67pdq5jlRNiRnQLUhrF1mLmorokJYQU4qdyfwLh7BwRh1Rq3iYfuiTfk";
-
-curl_setopt($ch, CURLOPT_URL,'https://api.sandbox.paypal.com/v1/oauth2/token');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
-curl_setopt($ch, CURLOPT_USERPWD, $clientId.":".$secret);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
-//curl_setopt($ch, CURLOPT_POSTFIELDS,"cmd=_notify-validate&".http_build_query($_POST));
-$response = curl_exec($ch);
+define("CLIENT_ID", "AdFIOOghKH7yHwoLS6g6JeZAczNBeNkUbt_hyMcLL0g6w5dfIUscHFhv-0IhxE6Y6IYaFEOUDi9voT_C");
+define("CLIENT_SECRET", "EHkHKQVIZtdF2B2KbjwInXFR67pdq5jlRNiRnQLUhrF1mLmorokJYQU4qdyfwLh7BwRh1Rq3iYfuiTfk");
+define("URI_SANDBOX", "https://api.sandbox.paypal.com/v1/");
 
 
-if(empty($response))die("Error: No response.");
-else
-{
-    $json = json_decode($response);
-   
-    file_put_contents("log.txt",$json->access_token);
-}
-
-
-curl_close($ch);
-
-
-
-if($response=="VERIFIED")
-{
-// $handel = fopen("log.txt","w");
-
-//     foreach($_POST as $key => $value)
-//         fwrite($handel,"$key => $value \r\n");
-
-//         fclose($handel);
-
-  $price = $_POST['mc_gross'];
-  $projectid =  $_POST['projectid'];
-  $userid =  $_POST['userid'];
-  $name =  $_POST['first_name'] . " " . $_POST['last_name'];
-  $email = get_user_email($userid);
-
-
-    file_put_contents("log.txt", "userid:" . $userid . " project:" . $projectid . " price:" . $price );
-
-    $mail = new PHPMailer();
-    $mail ->getFrom("urbanfund85@gmail.com","URBAN FUND");
-    $mail->addAddress($email, $name);
-    $mail->isHTML(true);
-    $mail->Subject = "תודה, תרומך לפרויקט התקבלה בהצלחה";
-    $mail->Body = "
-    <h1>תודה " . $name ."</h1>
-    <BR>
-    <p> תרומתך על סך: " . $price . " התקבלה בהצלחה
-    <BR>
-    תודה,
-    <BR>
-    URBAN FUND
-    ";
-    $mail->send();
+class paypal{
+    private $access_token;
+    private $token_type;
     
+    /**
+    * Constructor
+    *
+    * Handles oauth 2 bearer token fetch
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#authentication--headers
+    */
 
-
-
+    public function __construct(){
+        $postvals = "grant_type=client_credentials";
+        $uri = URI_SANDBOX . "oauth2/token";
+        
+        $auth_response = self::curl($uri, 'POST', $postvals, true);
+        $this->access_token = $auth_response['body']->access_token;
+        $this->token_type = $auth_response['body']->token_type;
+    }
     
+    /**
+    * Process Credit Card Payment
+    *
+    * Processes a credit card payment
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#create-a-payment
+    */
 
+    public function process_cc_payment($request){
+        $uri = URI_SANDBOX . "payments/payment";
+        return self::curl($uri, 'POST', json_encode($request));
+    }
+    
+    /**
+    * Process PayPal Payment
+    *
+    * Processes a PayPal payment and redirects the user to log in and authorize
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#create-a-payment
+    */
 
-    updateCurrentPrice($projectid,$price);
+    public function process_pp_payment($request){
+        $uri = URI_SANDBOX . "payments/payment";
+        $response = self::curl($uri, 'POST', json_encode($request));
+        $redirect = $response['body']->links[1]->href;
+        setcookie("id", $response['body']->id, time() + 3600);
+        header('Location: ' . $redirect);
+    }
+    
+    /**
+    * Fetch Payments
+    *
+    * Returns a series of previous payments made to the application account
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#list-payment-resources
+    */
 
+    public function fetch_payments($args){
+        $query_string = '';
+        if (count($args) > 0){ $query_string = '?' . http_build_query($args); }
+        
+        $uri = URI_SANDBOX . "payments/payment" . $query_string;
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
+    * Fetch Single Payment
+    *
+    * Returns a single payment made to the application account
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-payment-resource
+    */
+
+    public function fetch_single_payment($id){
+        $uri = URI_SANDBOX . "payments/payment/$id";
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
+    * Execute Approved Payment
+    *
+    * Executes an approved paypal payment
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#execute-an-approved-paypal-payment
+    */
+    
+    public function execute_payment($id, $request){
+        $uri = URI_SANDBOX . "payments/payment/$id/execute/";
+        setcookie("id","", time() - 3600);
+        return self::curl($uri, 'POST', json_encode($request));
+    }
+    
+    /**
+    * Capture Authorization
+    *
+    * Capture a previous payment authorization.  First need to call process_payment with an intent of "authorize"
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#capture-an-authorization
+    */
+    public function capture_authorization($id, $request){
+        $uri = URI_SANDBOX . "payments/authorization/$id/capture";
+        return self::curl($uri, 'POST', json_encode($request));
+    }
+    
+    /**
+    * Refund Sale
+    *
+    * Refunds a previous payment
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#refunds
+    */
+    public function refund_sale($sale_id){
+        $uri = URI_SANDBOX . "payments/sale/$sale_id/refund";
+        return self::curl($uri, 'POST', '{}');
+    }
+    
+    /**
+    * Fetch Refund
+    *
+    * Returns a single refund details object
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-refund
+    */
+    public function fetch_refund($id){
+        $uri = URI_SANDBOX . "refund/$id";
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
+    * Store Credit Card
+    *
+    * Stores a credit card value in the vault
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#store-a-credit-card
+    */
+    public function store_cc($cc_object){
+        $uri = URI_SANDBOX . "vault/credit-card";
+        return self::curl($uri, 'POST', json_encode($cc_object));
+    }
+    
+    /**
+    * Fetch Credit Card
+    *
+    * Fetches a credit card object from the vault
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-stored-credit-card
+    */
+    public function fetch_cc($cc_id){
+        $uri = URI_SANDBOX . "vault/credit-card/$cc_id";
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
+    * cURL
+    *
+    * Handles GET / POST requests for auth requests
+    * @link http://php.net/manual/en/book.curl.php
+    */
+    private function curl($url, $method = 'GET', $postvals = null, $auth = false){
+        $ch = curl_init($url);
+        
+        //if we are sending request to obtain bearer token
+        if ($auth){
+            $headers = array("Accept: application/json", "Accept-Language: en_US");
+            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($ch, CURLOPT_USERPWD, CLIENT_ID . ":" .CLIENT_SECRET);
+            curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        //if we are sending request with the bearer token for protected resources
+        } else {
+            $headers = array("Content-Type:application/json", "Authorization:{$this->token_type} {$this->access_token}");
+        }
+           
+        $options = array(
+            CURLOPT_HEADER => true,
+            CURLINFO_HEADER_OUT => true,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_VERBOSE => true,
+            CURLOPT_TIMEOUT => 10
+        );
+            
+            
+        if ($method == 'POST'){
+            $options[CURLOPT_POSTFIELDS] = $postvals;
+            $options[CURLOPT_CUSTOMREQUEST] = $method;
+        }
+        
+        curl_setopt_array($ch, $options);
+           
+        $response = curl_exec($ch);
+        $header = substr($response, 0, curl_getinfo($ch,CURLINFO_HEADER_SIZE));
+        $body = json_decode(substr($response, curl_getinfo($ch,CURLINFO_HEADER_SIZE)));
+        curl_close($ch);
+            
+        return array('header' => $header, 'body' => $body);
+    }
 }
-
-
-
-
-
-
-
-// //$price = $_POST['price'];
-// $projectid =  $_POST['projectid'];
-
-
-// updateCurrentPrice($projectid,$price);
-// header("Location: ../projectFund.php?projectid=$projectid");
- ?>
+?>
