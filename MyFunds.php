5acd4bb0b6b986181799d0a6b8219f9094f744ff
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-20 11:12:11 +0300

    u

--- MyFunds.php
+++ MyFunds.php
@@ -21,12 +21,12 @@
 <section>
     <div class="row">
         <?php
-        for($i=0;$i<sizeof($arrProjects);$i++)
+        for($i=0;$i<sizeof($arrFunds);$i++)
         {
             ?>
         <div class="card2"> 
             <?php
-            if($arrProjects[$i]->projectstatus==10)
+            if($arrFunds[$i]->projectstatus==10)
             {
                 ?>
                 <div class="notapprovedtext">הפרויקט לא מאושר</div>
@@ -36,21 +36,21 @@
             ?>
 
          
-            <div class="pictureCard2" style="background-image:url('images/project_types/<?php echo $arrProjects[$i]->projecttype; ?>_s.png');"></div>
+            <div class="pictureCard2" style="background-image:url('images/project_types/<?php echo $arrFunds[$i]->projecttype; ?>_s.png');"></div>
             <div class="descCard2">
-                <h3><?php echo getProjectType($arrProjects[$i]->projecttype);?></h3>
+                <h3><?php echo getProjectType($arrFunds[$i]->projecttype);?></h3>
                 <p class="loc">מיקום: רחוב
-                    <?php echo $arrProjects[$i]->loccity." ".$arrProjects[$i]->locstreet." ".$arrProjects[$i]->locnum;?>
+                    <?php echo $arrFunds[$i]->loccity." ".$arrFunds[$i]->locstreet." ".$arrFunds[$i]->locnum;?>
                 </p>
                 <div id="statusBar">
                     <div id="allStatus">
-                        <?php echo getStatusBarColors($arrProjects[$i]->projectstatus,1);?>
+                        <?php echo getStatusBarColors($arrFunds[$i]->projectstatus,1);?>
                     </div>
                     <div class="bar2">
-                        <p style="text-align:right; font-size:14px; margin-bottom:0px; color:#36ba2f"><?php echo $arrProjects[$i]->projectcost;?><i class="fas fa-shekel-sign"></i></p>
+                        <p style="text-align:right; font-size:14px; margin-bottom:0px; color:#36ba2f"><?php echo $arrFunds[$i]->projectcost;?><i class="fas fa-shekel-sign"></i></p>
                         <div id="mainBar2">
-                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="max-width:100%; width:<?php echo ($arrProjects[$i]->projectcurrentprice/$arrProjects[$i]->projectcost)*100;?>%;border-radius: 20px; ">
-                                <?php echo $arrProjects[$i]->projectcurrentprice?>
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="max-width:100%; width:<?php echo ($arrFunds[$i]->projectcurrentprice/$arrFunds[$i]->projectcost)*100;?>%;border-radius: 20px; ">
+                                <?php echo $arrFunds[$i]->projectcurrentprice?>
                             </div>
                         </div>
                     </div>
@@ -68,7 +68,7 @@
                     <i class="fab fa-facebook-square"></i>
                     </p>
                 </a>
-                <a href="projectFund.php?projectid=<?php echo $arrProjects[$i]->projectid; ?>" style="color:black;"><p id="view">לצפייה<i class="far fa-eye"></i></p></a>
+                <a href="projectFund.php?projectid=<?php echo $arrFunds[$i]->projectid; ?>" style="color:black;"><p id="view">לצפייה<i class="far fa-eye"></i></p></a>
             </div>
         </div>
     </div>
