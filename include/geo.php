abb12d261af914c61f996a260bc2461067bc2ca3
ido <navarro.ido@gmail.com>
2019-08-21 16:13:40 +0300

    u

--- include/geo.php
+++ include/geo.php
@@ -12,7 +12,7 @@ function geocode($address){
     $address = urlencode($address);
      
     // google map geocode api url
-    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyCK1iswODlN7nZQbJTB2viQH03KTUomNiE";
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={". $address ."}&key=AIzaSyCK1iswODlN7nZQbJTB2viQH03KTUomNiE";
  
     // get the json response
     $resp_json = file_get_contents($url);
