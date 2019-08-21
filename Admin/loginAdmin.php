bf5308a032ba3860b49b9e6de04eef60660bc0d6
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-21 09:45:01 +0300

    u

---
+++ Admin/loginAdmin.php
@@ -0,0 +1,64 @@
+<html>
+  <head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css\Admin_SidebarNav.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    require_once'../include/BLL.php';
+    session_start();
+    if(isset($_POST['submit'])){
+      $count=is_user_exist($_POST["userID"],$_POST["password"]);
+      if($count>0)
+      {
+        $_SESSION["userID"]=$_POST["userID"];
+        $_SESSION["type"]=get_user_type($_POST["userID"]);
+        if($_SESSION["type"] == 3){
+            header("Location:index.php");
+        }
+            
+        else{
+            ?>
+                <p> אין לך הרשאה להכנס למערכת</p>
+            <?php
+
+        }
+      }     
+    }
+  
+   ?>
+    
+  </head> 
+  <body>
+    <main id="mainLogin">
+        <div>
+            <div>
+                <img src="..\images\logo.png">
+            </div>
+            <div id="mainDiv">
+                <br>
+                <form action="" method="POST">
+                    <div>
+                        <div>
+                            <p style="font-size:18px; font-weight:bold;">תעודת זהות</p>
+                            <input required type="text" name="userID" maxlength="9"/>
+                        </div>
+                        <div>
+                            <p style="font-size:18px; font-weight:bold;">סיסמא</p>
+                            <input required type="password" name="password"/>
+                        </div>
+                        <br>
+                        <div>
+                            <input type="submit" class="login" value="שלח" name="submit">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    
+
+  </body>
+</html>
