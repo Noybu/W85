60a67f0e871cafca6793df62ae0504e75f4a8e45
Noybu <48063366+Noybu@users.noreply.github.com>
2019-08-24 17:18:00 +0300

    u

--- Admin/index.php
+++ Admin/index.php
@@ -52,7 +52,7 @@
 
         var data2 = google.visualization.arrayToDataTable([
          ['סטטוס פרויקט', 'כמות', { role: 'style' }],
-         ['נדחו', <?php echo getCountOfProjectStatus(10);?> , '#ff0000']
+         ['נדחו', <?php echo getCountOfProjectStatus(10);?> , '#ff0000'],
         ['ממתין לאישור', <?php echo getCountOfProjectStatus(0);?> , '#ffff00'],
         ['ממתין למכרז', <?php echo getCountOfProjectStatus(1);?> , '#999966'],
         ['ממתין למימון', <?php echo getCountOfProjectStatus(2);?> , '#ffff00'],
