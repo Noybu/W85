8465175c55556cae683f1bda238414281045133d
ido <navarro.ido@gmail.com>
2019-08-24 16:27:40 +0300

    u

--- include/addPayment.php
+++ include/addPayment.php
@@ -1,5 +1,5 @@
 <?php
-//header('Content-Type: charset=utf-8');
+
     require_once 'DAL.php';
     require_once 'BLL.php';
     use PHPMailer\PHPMailer\PHPMailer;
@@ -14,30 +14,18 @@ foreach($_POST as $key => $value) {
     $postdata .= $key . '=' . urlencode($value) . '&';
 }
 
-/* foreach($_POST as $keyPost => $valuePost)
-{ 
-    $postdata .=$keyPost." => ". urlencode($valuePost) . '&';
-} */
+
 
 $postdata .= 'cmd=_notify-validate';
 
-
-//$ch = curl_init();
-
-// $clientId = "AdFIOOghKH7yHwoLS6g6JeZAczNBeNkUbt_hyMcLL0g6w5dfIUscHFhv-0IhxE6Y6IYaFEOUDi9voT_C";
-// $secret = "EHkHKQVIZtdF2B2KbjwInXFR67pdq5jlRNiRnQLUhrF1mLmorokJYQU4qdyfwLh7BwRh1Rq3iYfuiTfk";
-
 $ch = curl_init('https://www.paypal.com/cgi-bin/webscr');
-//curl_setopt($ch, CURLOPT_URL,'https://api.sandbox.paypal.com/v1/oauth2/token');
 curl_setopt ($ch, CURLOPT_HEADER, 0);
 curl_setopt ($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata);
-//curl_setopt($ch, CURLOPT_USERPWD, $clientId.":".$secret);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
-//curl_setopt($ch, CURLOPT_POSTFIELDS,"cmd=_notify-validate&".http_build_query($_POST));
+
 $response = curl_exec($ch);
 
 if($response == 'VERIFIED') {
@@ -60,7 +48,7 @@ if($response == 'VERIFIED' && $_POST['payment_status'] == 'Completed')
     $price = $_POST['mc_gross'];
     $projectid =  $_POST['option_selection1'];
     $userid =  $_POST['option_selection2'];
-    $name =  $_POST['first_name'] . " " . $_POST['last_name'];
+    $name = get_user_name($userid);
     $email = get_user_email($userid);
     $currency= $_POST['mc_currency'];
 
@@ -74,9 +62,9 @@ if($response == 'VERIFIED' && $_POST['payment_status'] == 'Completed')
     $mail->addAddress($email, $name);
     $mail->isHTML(true);
     
-    $mail->Subject = "תודה, תרומך לפרויקט התקבלה בהצלחה";
+    $mail->Subject = "תרומך לפרויקט התקבלה בהצלחה";
     $mail->Body = "
-    <h1>תודה " . $name ."</h1>
+    <div style='direction:rtl;'><h1>תודה " . $name ."</h1>
     <BR>
     <p> תרומתך על סך: " . $price . $currency. " התקבלה בהצלחה
     <BR>
@@ -86,7 +74,7 @@ if($response == 'VERIFIED' && $_POST['payment_status'] == 'Completed')
     תודה,
     <BR>
     URBAN FUND
-    ";
+    </div>";
     $mail->send();
    
     file_put_contents('log.txt', '*****Message has been sent*****' . '\r\n', FILE_APPEND);
@@ -95,16 +83,4 @@ if($response == 'VERIFIED' && $_POST['payment_status'] == 'Completed')
 
 }
 
-
-
-
-
-
-
-// //$price = $_POST['price'];
-// $projectid =  $_POST['projectid'];
-
-
-// updateCurrentPrice($projectid,$price);
-// header("Location: ../projectFund.php?projectid=$projectid");
  ?>
