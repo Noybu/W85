bac9bc26cf32aad7ea8cf9a1f479be37da7ad7f6
Noybu <48063366+Noybu@users.noreply.github.com>
2019-09-01 16:30:26 +0300

    update

--- include/update.php
+++ include/update.php
@@ -30,7 +30,7 @@ switch ($type) {
     case "doneProject":
     {
         updateProjectStatus($_GET['id'],4);
-        updateProjectFinalDate($_GET['projectid']);
+        updateProjectFinalDate($_GET['id']);
         header("Location:../myBids.php");
         break;
     }
