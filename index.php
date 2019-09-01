caebcd932695de1e879a00ccf734cacb972277e0
Noybu <48063366+Noybu@users.noreply.github.com>
2019-09-01 14:52:26 +0300

    update

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 </div>
             </div>
         </section>
-        <div class="row center sm">
+        <div class="row sm">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mx-auto center">
                     <a href="newProject.php"><button class="actionButton">התחילו עכשיו</button><a>
                 </div>
