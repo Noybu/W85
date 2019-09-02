b2b7dd764208a8fb8f15b96d1b4098443de9b051
Noybu <48063366+Noybu@users.noreply.github.com>
2019-09-02 15:11:28 +0300

    Update

--- login/include/register.php
+++ login/include/register.php
@@ -110,17 +110,17 @@ switch ($command) {
             $_SESSION["type"]=get_user_type($_POST["userID"]);
            
             //חזרה לדף האחרון
-         //   $page = $_SERVER["REQUEST_URI"];
-           // $serverName=$_SERVER['SERVER_NAME'];
-        //    $curPage=$_SESSION['page'];
-        //    if($page) //אם לא קיים דף אחרו- להחזיר לדף הביתה
-        //    {
-              header("Location: ../../index.php");
-         //   }
-         //   else{
-          //    header("Location:'http://'.$serverName.$curPage");
+              $page = $_SERVER["REQUEST_URI"];
+             $serverName=$_SERVER['SERVER_NAME'];
+                $curPage=$_SESSION['page'];
+                if($page) //אם לא קיים דף אחרו- להחזיר לדף הביתה
+                {
+                    header("Location: ../../index.php");
+               }
+               else{
+                   header("Location:'http://'.$serverName.$curPage");
+                }
             }
-        
         //אם סיסמא לא נכונה 
         else
             header("Location: ../signIn.php?Error=IncorrectUsernameOrPassword");
