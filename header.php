066d4d70e3fc69158287314316a4912acb33b326
Noybu <48063366+Noybu@users.noreply.github.com>
2019-09-02 14:00:01 +0300

    update

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <?php
 include_once("include/BLL.php");
   session_start();
-  $_SESSION['page'] = $page;
-  echo $page;
+  $_SESSION['page'] = $_SERVER["REQUEST_URI"];
+  echo $_SESSION['page'];
   if(isset($_GET['logout']))
   {
     if($_GET['logout']=='yes')
